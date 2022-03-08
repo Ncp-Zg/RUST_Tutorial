cd29a629165959f27149f1849d478b49b042a62d
Necip <ncptzgn@gmail.com>
2022-03-08 19:26:44 +0300

    impl blocks

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,18 +4,48 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn can_hold(&self, other: &Rectangle) ->bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
+    
+
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
         width:dbg!(30 * scale),
         height:50,
     };
+    let rect2 = Rectangle {
+        width:10,
+        height:50,
+    };
+    let rect3 = Rectangle {
+        width:10,
+        height:10,
+    };
 
     println!(
-        "The area is the rectangle is {:#?} square pixels.", rect1
-    )
+        "The area is the rectangle is {:#?} square pixels.", rect1.width()
+    );
+    println!(
+        "Can rect1 hold rect2? {}", rect1.can_hold(&rect2)
+    );
+    println!(
+        "Can rect1 hold rect3? {}", rect1.can_hold(&rect3)
+    );
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
